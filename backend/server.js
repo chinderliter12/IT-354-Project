33591c7c5113302077222562980cac5037eb168d
Cameron <chinderliter12@gmail.com>
2026-04-27 03:52:22 -0500

    Rearranged files and improved pathing

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -15,18 +16,17 @@ mongoose.connect(process.env.MONGO_URI)
 // Routes
 const authRoute = require('./routes/auth');
 app.use('/api/auth', authRoute);
-
 const bookingRoutes = require('./routes/bookingRoutes');
 app.use('/api/booking', bookingRoutes);
-
-
 const courseRoutes = require('./routes/courseRoutes');
 app.use('/api/courses', courseRoutes);
 
+//Brings user to main page
+app.use(express.static(path.join(__dirname, '../frontend')));
 app.get('/', (req, res) => {
-  res.send("Server is running");
+  res.redirect('/views/homePage.html');
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
