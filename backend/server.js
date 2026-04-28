a7a1a4751009cfb457d32d15d2806b6dbc717b3a
Cameron <chinderliter12@gmail.com>
2026-04-27 21:18:28 -0500

    Adjusting server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
@@ -15,10 +13,11 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Routes
 const authRoute = require('./routes/auth');
-app.use('/api/auth', authRoute);
 const bookingRoutes = require('./routes/bookingRoutes');
-app.use('/api/booking', bookingRoutes);
 const courseRoutes = require('./routes/courseRoutes');
+
+app.use('/api/auth', authRoute);
+app.use('/api/booking', bookingRoutes);
 app.use('/api/courses', courseRoutes);
 
 //Brings user to main page
@@ -27,6 +26,6 @@ app.get('/', (req, res) => {
   res.redirect('/views/homePage.html');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
